e4eb8aabd4d7d82e9eefb73273a9552d4376d768
Hector <63668294+stanHector@users.noreply.github.com>
2022-07-26 09:50:33 +0100

    initiated project

---
+++ src/main/java/service/ricotunes/giftcards/GiftcardsApplication.java
@@ -0,0 +1,13 @@
+package service.ricotunes.giftcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/service/ricotunes/giftcards/GiftcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package service.ricotunes.giftcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
